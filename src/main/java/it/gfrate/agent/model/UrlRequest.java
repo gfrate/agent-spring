a5dd2ada5e02d95a1ae3a1a5782c10da94983208
gfrate <giovannifrate2@icloud.com>
2023-12-02 00:21:01 +0100

    add urlrequest dto for creating a shortened link

---
+++ src/main/java/it/gfrate/agent/model/UrlRequest.java
@@ -0,0 +1,20 @@
+package it.gfrate.agent.model;
+
+public class UrlRequest {
+    private String originalUrl;
+
+    public UrlRequest() {
+    }
+
+    public UrlRequest(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+}
