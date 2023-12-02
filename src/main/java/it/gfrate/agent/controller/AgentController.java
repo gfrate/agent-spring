9a5a821c8f11a11bb57b597b1ff09543a46e8610
gfrate <giovannifrate2@icloud.com>
2023-12-02 01:25:46 +0100

    add getOriginalUrl with redirect as return type

--- src/main/java/it/gfrate/agent/controller/AgentController.java
+++ src/main/java/it/gfrate/agent/controller/AgentController.java
@@ -3,8 +3,17 @@ package it.gfrate.agent.controller;
 import it.gfrate.agent.model.UrlRequest;
 import it.gfrate.agent.model.UrlResponse;
 import it.gfrate.agent.service.AgentService;
+import jakarta.servlet.http.HttpServlet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
 
 @RestController
 public class AgentController {
@@ -15,5 +24,10 @@ public class AgentController {
     @PostMapping("/")
     public UrlResponse createShortUrl(@RequestBody UrlRequest urlRequest) {
         return agentService.createShortUrl(urlRequest);
+    }
+
+    @GetMapping("/{shortenedUrl}")
+    public RedirectView getOriginalUrl(@PathVariable("shortenedUrl") String shortenedUrl) {
+        return new RedirectView(agentService.getOriginalUrl(shortenedUrl));
     }
 }
