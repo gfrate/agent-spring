97ac2b96310e8e9d96cab24dc8c66e34cef3b4f7
gfrate <giovannifrate2@icloud.com>
2023-12-02 03:28:58 +0100

    remove unused imports

--- src/main/java/it/gfrate/agent/controller/AgentController.java
+++ src/main/java/it/gfrate/agent/controller/AgentController.java
@@ -3,17 +3,9 @@ package it.gfrate.agent.controller;
 import it.gfrate.agent.model.UrlRequest;
 import it.gfrate.agent.model.UrlResponse;
 import it.gfrate.agent.service.AgentService;
-import jakarta.servlet.http.HttpServlet;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpResponse;
 
 @RestController
 public class AgentController {
